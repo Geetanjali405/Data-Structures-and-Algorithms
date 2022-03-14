0d87d7da1da11b6e8729bd909ad2ac3b50351c8c
Geetanjali <geetbgs04@gmail.com>
2022-03-15 01:24:22 +0530

    Updated repository

---
+++ 08.Matrix/1printsnake.cpp
@@ -0,0 +1,28 @@
+// print a matrix in snake pattern
+#include <iostream>
+using namespace std;
+void printsnake(int arr[][4])
+{
+    for (int i = 0; i < 4; i++)
+    {
+        if (i % 2 == 0)
+        {
+            for (int j = 0; j < 4; j++)
+                cout << arr[i][j] << "  ";
+        }
+        else
+        {
+            for (int j = 3; j >= 0; j--)
+            {
+                cout << arr[i][j] << "  ";
+            }
+        }
+    }
+}
+int main()
+{
+    int arr[4][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+    printsnake(arr);
+
+    return 0;
+}
