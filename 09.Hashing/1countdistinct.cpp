88e042fd06d8f748f5f17bae7188a0f85b95ba19
Geetanjali <geetbgs04@gmail.com>
2022-03-18 01:45:41 +0530

    DSA

---
+++ 09.Hashing/1countdistinct.cpp
@@ -0,0 +1,18 @@
+// count disting elements in an array
+#include <bits/stdc++.h>
+using namespace std;
+int countdistinct(int arr[], int n)
+{
+    unordered_set<int> s;
+    for (int i = 0; i < n; i++)
+        s.insert(arr[i]);
+    return s.size();
+}
+int main()
+{
+    int arr[] = {10, 20, 10, 20, 30};
+    int n = 5;
+    int x = countdistinct(arr, n);
+    cout << x << endl;
+    return 0;
+}
---
+++ 09.Hashing/2frequencies.cpp
@@ -0,0 +1,19 @@
+// 2.frequencies of array elements
+// count disting elements in an array
+#include <bits/stdc++.h>
+using namespace std;
+void frequency(vector<int> arr, int n)
+{
+    unordered_map<int, int> h;
+    for (int x : arr)
+        h[x]++;
+    for (auto itr = h.begin(); itr != h.end(); itr++)
+        cout << itr->first << " " << itr->second << endl;
+}
+int main()
+{
+    vector<int> arr = {10, 20, 10, 20, 30};
+    int n = 5;
+    frequency(arr, n);
+    return 0;
+}
